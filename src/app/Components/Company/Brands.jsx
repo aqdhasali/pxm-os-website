2ddf1c59e50962bd6ce53bf5bd1bc58b265bb0f3
aqdhasali <aqdhas@commercializer.global>
2024-11-28 12:58:48 +0530

    made learn more and view all functional

--- src/app/Components/Company/Brands.jsx
+++ src/app/Components/Company/Brands.jsx
@@ -1,21 +1,69 @@
+import { useState } from "react"
+
 export default function Brands(){
+
+    const [boxHeight, setBoxHeight] = useState('180px')
+
+
+    const handleClick = () =>{
+        setBoxHeight('auto')
+    }
+
     return(
         <div className="flex flex-col gap-5 m-5 p-5 w-auto bg-[#ffffff] rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-2xl ">
             <h2 className="text-xl font-bold tracking-tight">Brands</h2>
 
-            <div className="flex justify-center items-center  gap-x-5">
+            <div className="grid grid-cols-2 items-center overflow-hidden gap-x-5" style={{height: boxHeight}}>
                 <div className="flex flex-col items-center">
-                    <img className="w-32" src="sayerlack-logo.png"></img>
+                    <img className="w-32" src="Logo-of-SAYERLACK-INNOVATIVE-WOOD-SOLUTIONS.jpg"></img>
                     <p>Sayerlack</p>
                 </div>
 
                 <div className="flex flex-col items-center">
-                    <img className="w-32" src="hermanmiller-logo2.png"></img>
-                    <p>Herman Miller</p>
+                    <img className="w-32" src="Borma-Wachs-Logo.jpg"></img>
+                    <p>Borma Wachs</p>
                 </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="JAT-White-logo-327x327.jpg"></img>
+                    <p>White By JAT</p>
+                </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="Logo-of-Brushmaster-paint-brushes.jpg"></img>
+                    <p>Brushmaster</p>
+                </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="Logo-of-J-CHEM.jpg"></img>
+                    <p>J CHEM</p>
+                </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="Logo-of-Petal-paint.jpg"></img>
+                    <p>Petal Paint</p>
+                </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="masters-thumbnail.jpg"></img>
+                    <p>Masters</p>
+                </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="logo-of-wallz.jpg"></img>
+                    <p>Wallz</p>
+                </div>
+                
+                <div className="flex flex-col items-center">
+                    <img className="w-32" src="Logo-of-Harris-paint-brushes.jpg"></img>
+                    <p>Harris</p>
+                </div>
+
+
+
             </div>
 
-            <div className="flex items-center justify-center bg-green w-full rounded-md text-l text-white h-12">View All</div>
+            <div className="flex items-center justify-center bg-green w-full rounded-md text-l text-white h-12" onClick={() => handleClick()}>View All</div>
 
         </div>
     )
--- src/app/Components/Product/SayerlackContent.jsx
+++ src/app/Components/Product/SayerlackContent.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
+
 export default function SayerlackContent(){
+
+    const [boxHeight, setboxHeight] = useState('200px')
+    
+
+
+    const handleClick = () => {
+        setboxHeight('auto')
+    }
+
     return(
-        <div className="flex flex-col md-screen sm-screen-2 sm-screen lg-screen gap-2 p-5 m-6 bg-[#ffffff] rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-2xl w-auto p-2">
+        <div className="flex flex-col md-screen sm-screen-2 sm-screen lg-screen gap-2 p-5 m-6 bg-[#ffffff] rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-2xl w-auto p-2 overflow-hidden" >
             <h2 className="text-left text-xl font-bold">About Sayerlack Water Base</h2>
-            <p className="font-light">Finishing off wood requires careful planning to ensure the finished piece looks attractive, performs well and meets safety and environmental requirements. Sayerlack Waterbase has unique features such as non-fading/ rotting, resistance to rainwater/UV rays and salt spray, because doors, window frames and wood work exposed to the outdoor environment needs the best wood preservative applicant available in the market.</p>
-            <div className="flex items-center justify-center bg-green w-full rounded-md text-l text-white h-12">Learn More</div>
+            <p className="font-light text-jusitfy overflow-hidden" style={{ height: boxHeight }}>Sayerlack is the flagship brand distributed by JAT Holdings PLC. Initiated in 1954, Sayerlack Wood Coatings is an Italian origin brand owned by Sherwin Williams that provides the most innovative and highest quality wood coatings in the industry. Sherwin Williams is recognized as the largest producer of wood coatings in the world due to the new technologies it adopts and for its commitment to research and development.
+                With 28 years of experience in distributing Sayerlack in Sri Lanka and across South Asia, JAT Holdings is recognized as the number 1 partner for Sayerlack in the world. JAT Holdings PLC was the pioneer to introduce Waterbase wood coatings to Sri Lanka. Water-based coatings revolutionized the industry by giving consumers an environmentally friendly alternative with 95% lower harmful emissions than synthetic products, ensuring workers’ health and safety.
+                Sayerlack is the market leader in wood coatings and a trusted brand that is loved amongst Sri Lankan customers in the residential and industrial sectors. The brand has been recognized amongst the Top 100 Most Valuable Consumer Brands in Sri Lanka in the Brands Annual 2022 list by LMD.
+            </p>
+            <div className="flex items-center justify-center bg-green w-full rounded-md text-l text-white h-12"  onClick={ () => handleClick()}>Learn More</div>
         </div>
     )
 }

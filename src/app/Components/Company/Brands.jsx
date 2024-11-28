8560208a65725cf7d24ae64650a43f66b521dc49
aqdhasali <aqdhas@commercializer.global>
2024-11-28 16:36:35 +0530

    improvements

--- src/app/Components/Company/Brands.jsx
+++ src/app/Components/Company/Brands.jsx
@@ -24,48 +24,48 @@ export default function Brands(){
         <div className="flex flex-col gap-5 m-5 p-5 w-auto bg-[#ffffff] rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-2xl ">
             <h2 className="text-xl font-bold tracking-tight">Brands</h2>
 
-            <div className="grid grid-cols-2 items-center overflow-hidden gap-x-5" style={{height: boxHeight}}>
-                <div className="flex flex-col items-center">
+            <div className="grid grid-cols-2 items-center overflow-hidden gap-7" style={{height: boxHeight}}>
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="Logo-of-SAYERLACK-INNOVATIVE-WOOD-SOLUTIONS.jpg"></img>
                     <p>Sayerlack</p>
                 </div>
 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="Borma-Wachs-Logo.jpg"></img>
                     <p>Borma Wachs</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="JAT-White-logo-327x327.jpg"></img>
                     <p>White By JAT</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="Logo-of-Brushmaster-paint-brushes.jpg"></img>
                     <p>Brushmaster</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="Logo-of-J-CHEM.jpg"></img>
                     <p>J CHEM</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="Logo-of-Petal-paint.jpg"></img>
                     <p>Petal Paint</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="masters-thumbnail.jpg"></img>
                     <p>Masters</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="logo-of-wallz.jpg"></img>
                     <p>Wallz</p>
                 </div>
                 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center rounded-lg shadow-lg">
                     <img className="w-32" src="Logo-of-Harris-paint-brushes.jpg"></img>
                     <p>Harris</p>
                 </div>
